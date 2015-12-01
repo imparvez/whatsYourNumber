6aa32ba5c1e87454118d0e53627442ec66fadbd4
Parvez shaikh <parvezshaikhj@gmail.com>
2015-12-01 23:29:35 +0530

    Initial commit

--- whatNumber/ViewController.swift
+++ whatNumber/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Logic Of App
+    //1. To create a random number
+    
+    var correct = arc4random_uniform(5)//Correct is now INT value/dataType
 
+    @IBOutlet weak var message: UILabel!
+    
+    @IBOutlet weak var guessField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //printing correct variable on view load
+        print(correct)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func checkButton(sender: UIButton) {
+        var guess = guessField.text//Taking input in strng format
+        
+        //Creating a variable which will convert random number i.e correct variable into STRING and after that we can compare it.
+        var correctGuess = String(correct)//Making correct variable in a stringify version
+        
+        if(guess == correctGuess) {
+            message.text = "Correct ANswer"
+        } else {
+            message.text = "Wrong guess"
+        }
+        
+    }
 
 }
 
